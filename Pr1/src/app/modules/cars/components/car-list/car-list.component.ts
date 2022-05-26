71b525efa32e66929ce1d005d3c62e4a14670b62
Alexander Cherkas <AlexanderCherkas@coherentsolutions.com>
2022-05-26 18:43:48 +0300

    somethink craate

--- Pr1/src/app/modules/cars/components/car-list/car-list.component.ts
+++ Pr1/src/app/modules/cars/components/car-list/car-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ICar } from 'src/app/interfaces/car';
 
 @Component({
   selector: 'app-car-list',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarListComponent implements OnInit {
 
+  @Input() car: any;
+  @Input() carLikesStatus: string = "primary"
+
+  @Output() onLikes = new EventEmitter<boolean>();
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  isLikes(car: ICar): void {
+
+    if( this.carLikesStatus == "primary"){
+      this.carLikesStatus = "warn"
+    } else{
+      this.carLikesStatus = "primary"
+    }
+    this.onLikes.emit();
+
+
+
+  }
 }
--- Pr1/src/app/modules/cars/services/cardata.service.ts
+++ Pr1/src/app/modules/cars/services/cardata.service.ts
@@ -8,45 +8,47 @@ export class CardataService {
 
   constructor() { }
 
-  public getUsers(): ICar[] {
+  public getCars(): ICar[] {
     return [
       {
         id: 100000,
         name: 'Ford',
         color: 'White',
-        releaseYear: 2016,
+        releaseYear: 1969,
         number: 4546,
-        imageUrl: '',
+        imageUrl: 'https://www.allcarz.ru/wp-content/uploads/2019/11/foto-ringbrothers-mach-1_05.jpg',
 
        },
        {
-        id: 100000,
-        name: 'Ford',
-        color: 'White',
-        releaseYear: 2016,
-        number: 4546,
-        imageUrl: '',
+        id: 200000,
+        name: 'Volkswagen',
+        color: 'Red',
+        releaseYear: 2020,
+        number: 3565,
+        imageUrl: 'https://www.allcarz.ru/wp-content/uploads/2013/08/foto-vw-golf-7-r_02-650x433.jpg',
        },
        {
-        id: 100000,
-        name: 'Ford',
-        color: 'White',
-        releaseYear: 2016,
-        number: 4546,
-        imageUrl: '',
+        id: 300000,
+        name: 'Audi',
+        color: 'Green',
+        releaseYear: 1994,
+        number: 1234,
+        imageUrl: 'https://auto.vercity.ru/gallery/img/automobiles/Audi/1995%20Audi%20RS2%20Avant%20Tropic%20Green%20(8C_B4)/900x/1565772642.jpg',
        },
        {
-        id: 100000,
-        name: 'Ford',
-        color: 'White',
-        releaseYear: 2016,
-        number: 4546,
-        imageUrl: '',
+        id: 400000,
+        name: 'Volvo',
+        color: 'Black',
+        releaseYear: 2022,
+        number: 43534,
+        imageUrl: 'https://www.allcarz.ru/wp-content/uploads/2021/05/foto-s90-2022_00.jpg',
        }
 
     ];
 
   }
+
+
 }
 
 
