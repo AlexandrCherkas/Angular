19f04e8a35bead21ff9da0ecdf848a0c051d8874
Alexander Cherkas <AlexanderCherkas@coherentsolutions.com>
2022-06-23 17:17:43 +0300

    create 'save' and 'export' button

--- Pr1/src/app/modules/shared/components/card/card.component.ts
+++ Pr1/src/app/modules/shared/components/card/card.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { fromEvent } from 'rxjs';
 import { ICar } from 'src/app/modules/cars/interface/car';
 import { IUser } from 'src/app/modules/users/interface/user';
+import { UserdataService } from 'src/app/modules/users/services/userdata.service';
 
 
 @Component({
@@ -14,15 +16,38 @@ export class CardComponent implements OnInit {
   @Input() id!: any;
 
   @Output() toFavoriteEvent = new EventEmitter();
+  @Output() toSaveEvent = new EventEmitter();
+  @Output() toExportToExcelEvent = new EventEmitter();
+
+  constructor( private userService: UserdataService) {}
+
+  ngOnInit(): void {
+
+    // fromEvent(document.getElementById('save'), 'click')
+    // .pipe(
+    //   switchMap(() => {
+    //     return this.usersService
+    //       .getUsers(this.pageIndex, this.pageSize)
+    //       .pipe(switchMap((res) => res));
+    //   })
+    // )
+    // .subscribe((data) => data);
 
 
-  constructor() {}
 
-  ngOnInit(): void { }
+  }
 
   addToFavorite(): void {
     this.toFavoriteEvent.emit();
     this.isLikes = !this.isLikes;
+  }
+
+  save(): void {
+    this.toSaveEvent.emit();
+  }
+
+  exportToExcel(): void {
+    this.toExportToExcelEvent.emit();
   }
 
 }
